6331e8a2954fa0415c74dc83089999fb3a1318ed
Carlos Alberto Castelo Elias Filho <cacefmail@gmail.com>
2016-09-06 16:20:13 -0300

    State de exemplo com exemplos de clique

--- src/ts/example-state.ts
+++ src/ts/example-state.ts
@@ -6,5 +6,15 @@ export class ExampleState extends Phaser.State {
   create() {
     let logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
+    logo.inputEnabled = true;
+    logo.events.onInputOver.add(() => {
+      this.game.add.tween(logo.scale).to({x: 1.2, y: 1.2}, 100, Phaser.Easing.Linear.None, true);
+    });
+    logo.events.onInputOut.add(() => {
+      this.game.add.tween(logo.scale).to({x: 1, y: 1}, 100, Phaser.Easing.Linear.None, true);
+    });
+    logo.events.onInputDown.add(() => {
+      this.game.add.tween(logo).to({angle: 360}, 500, Phaser.Easing.Cubic.Out, true);
+    })
   }
 }
